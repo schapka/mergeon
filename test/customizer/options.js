d2fa847d2f55f95ca1d30a7e666bc74901bd6c5e
René Schapka <rene@schapka.info>
2017-12-19 04:05:45 +0100

    Add prettier

--- test/customizer/options.js
+++ test/customizer/options.js
@@ -3,7 +3,7 @@ const _ = require('lodash');
 
 module.exports = {
   entry: path.resolve(__dirname, 'data', 'entry.json'),
-  mergeCustomizer: function (objValue, srcValue) {
+  mergeCustomizer: function(objValue, srcValue) {
     if (_.isArray(objValue)) {
       return objValue.concat(srcValue);
     }
